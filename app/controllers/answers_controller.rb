d8b3c8d9cca0606758589207a7b5f21a03207b9a
RenanBa <renanbarbosadesouza@gmail.com>
2016-07-22 16:56:19 -0700

    Add answer to que questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,4 @@
-get '/answers/new' do
-
+post '/answers/:id' do
+    @answer = Answer.create(answer: params[:answer], question_id: params[:id], user_id: session[:user_id])
+    redirect "/questions/#{params[:id]}"
 end
