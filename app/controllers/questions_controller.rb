528710da17985c01a85df172a73da1444c7dd863
RenanBa <renanbarbosadesouza@gmail.com>
2016-07-22 17:18:23 -0700

    Make question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,14 @@
+get '/questions/new' do
+  erb :'/questions/new'
+end
+
+
 get '/questions/:id' do
 	@question = Question.find(params[:id])
 	erb :"/questions/index"
+end
+
+post '/questions/new' do
+  @question = Question.create(title: params[:title], question: params[:question], user_id: session[:user_id])
+  redirect :"/"
 end
